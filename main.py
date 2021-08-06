5da37a2422770b7e0cd9d2a62e3e607dc5a8300c
Dmitry Kalugin <kalugin180806@gmail.com>
2021-08-07 00:14:21 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+chislo = input()
+chislo_na_schote_drevnih_shizov = ""
+cifri_scheta_drevnih_shizov = "ноль, целковый, чекушка, порнушка, пердушка, засирушка, жучок, мудачок, хуй на воротничок, дурачок, ВСЕ".split(", ")
+for i in chislo: chislo_na_schote_drevnih_shizov+=cifri_scheta_drevnih_shizov[int(i)]
+print(chislo_na_schote_drevnih_shizov)
